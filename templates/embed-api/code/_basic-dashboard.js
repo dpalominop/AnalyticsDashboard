b60eb5d7c418c30b73f3451af12d4845a5dc469f
Daniel Palomino <dapalominop@gmail.com>
2016-11-21 15:51:17 -0500

    Creación de cuadro de comparación temporal en vista Basico.

--- templates/embed-api/code/_basic-dashboard.js
+++ templates/embed-api/code/_basic-dashboard.js
@@ -1,3 +1,7 @@
+// == NOTE ==
+// This code uses ES6 promises. If you want to use this code in a browser
+// that doesn't supporting promises natively, you'll have to include a polyfill.
+
 gapi.analytics.ready(function() {
 
   /**
@@ -12,22 +16,68 @@ gapi.analytics.ready(function() {
 
 
   /**
-   * Create a new ViewSelector instance to be rendered inside of an
-   * element with the id "view-selector-container".
+   * Create a new ActiveUsers instance to be rendered inside of an
+   * element with the id "active-users-container" and poll for changes every
+   * five seconds.
    */
-  var viewSelector = new gapi.analytics.ViewSelector({
-    container: 'view-selector-container'
+  var activeUsers = new gapi.analytics.ext.ActiveUsers({
+    container: 'active-users-container',
+    pollingInterval: 5
   });
 
-  // Render the view selector to the page.
-  viewSelector.execute();
+  /**
+   * Add CSS animation to visually show the when users come and go.
+   */
+  activeUsers.once('success', function() {
+    var element = this.container.firstChild;
+    var timeout;
+
+    this.on('change', function(data) {
+      var element = this.container.firstChild;
+      var animationClass = data.delta > 0 ? 'is-increasing' : 'is-decreasing';
+      element.className += (' ' + animationClass);
+
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        element.className =
+            element.className.replace(/ is-(increasing|decreasing)/g, '');
+      }, 3000);
+    });
+  });
 
 
   /**
-   * Create a new DataChart instance with the given query parameters
-   * and Google chart options. It will be rendered inside an element
-   * with the id "chart-container".
+   * Create a new ViewSelector2 instance to be rendered inside of an
+   * element with the id "view-selector-container".
    */
+  var viewSelector = new gapi.analytics.ext.ViewSelector2({
+    container: 'view-selector-container',
+  })
+  .execute();
+
+
+  /**
+   * Update the activeUsers component, the Chartjs charts, and the dashboard
+   * title whenever the user changes the view.
+   */
+  viewSelector.on('viewChange', function(data) {
+    var title = document.getElementById('view-name');
+    title.innerHTML = data.property.name + ' (' + data.view.name + ')';
+
+    // Start tracking active users for this view.
+    activeUsers.set(data).execute();
+
+    // Render all the of charts for this view.
+    dataChart.set({query: {ids: data.ids}}).execute();
+    newUsersDataChart.set({query: {ids:data.ids}}).execute();
+    usersDataChart.set({query: {ids:data.ids}}).execute();
+    sessionsByBrowsersDataChart.set({query: {ids:data.ids}}).execute();
+    topLandingDataChart.set({query: {ids:data.ids}}).execute();
+    allPagesDataChart.set({query: {ids:data.ids}}).execute();
+    renderYearOverYearOverYearChart(data.ids);
+  });
+
+
   var dataChart = new gapi.analytics.googleCharts.DataChart({
     query: {
       metrics: 'ga:sessions',
@@ -145,17 +195,145 @@ gapi.analytics.ready(function() {
     }
   });
 
+/**
+ * 
+ */
+
+  function renderYearOverYearOverYearChart(ids) {
+
+    // Adjust `now` to experiment with different days, for testing only...
+    var now = moment(); // .subtract(3, 'day');
+
+    var thisYear = query({
+      'ids': ids,
+      'dimensions': 'ga:month,ga:nthMonth',
+      'metrics': 'ga:users',
+      'start-date': moment(now).date(1).month(0).format('YYYY-MM-DD'),
+      'end-date': moment(now).format('YYYY-MM-DD')
+    });
+
+    var lastYear = query({
+      'ids': ids,
+      'dimensions': 'ga:month,ga:nthMonth',
+      'metrics': 'ga:users',
+      'start-date': moment(now).subtract(1, 'year').date(1).month(0)
+          .format('YYYY-MM-DD'),
+      'end-date': moment(now).date(1).month(0).subtract(1, 'day')
+          .format('YYYY-MM-DD')
+    });
+
+    var beforeLastYear = query({
+      'ids': ids,
+      'dimensions': 'ga:month,ga:nthMonth',
+      'metrics': 'ga:users',
+      'start-date': moment(now).subtract(2, 'year').date(1).month(0)
+          .format('YYYY-MM-DD'),
+      'end-date': moment(now).subtract(1, 'year').date(1).month(0).subtract(1, 'day')
+          .format('YYYY-MM-DD')
+    });
+
+    Promise.all([thisYear, lastYear, beforeLastYear]).then(function(results) {
+      var data1 = results[0].rows.map(function(row) { return +row[2]; });
+      var data2 = results[1].rows.map(function(row) { return +row[2]; });
+      var data3 = results[2].rows.map(function(row) { return +row[2]; });
+      var labels = ['Jan','Feb','Mar','Apr','May','Jun',
+                    'Jul','Aug','Sep','Oct','Nov','Dec'];
+
+      // Ensure the data arrays are at least as long as the labels array.
+      // Chart.js bar charts don't (yet) accept sparse datasets.
+      for (var i = 0, len = labels.length; i < len; i++) {
+        if (data1[i] === undefined) data1[i] = null;
+        if (data2[i] === undefined) data2[i] = null;
+        if (data3[i] === undefined) data3[i] = null;
+      }
+
+      var data = {
+        labels : labels,
+        datasets : [
+          {
+            label: 'Before Last Year',
+            fillColor : 'rgba(120,120,120,0.5)',
+            strokeColor : 'rgba(120,120,120,1)',
+            data : data3
+          },
+          {
+            label: 'Last Year',
+            fillColor : 'rgba(220,220,220,0.5)',
+            strokeColor : 'rgba(220,220,220,1)',
+            data : data2
+          },
+          {
+            label: 'This Year',
+            fillColor : 'rgba(151,187,205,0.5)',
+            strokeColor : 'rgba(151,187,205,1)',
+            data : data1
+          }
+        ]
+      };
+
+      new Chart(makeCanvas('chart-6-container')).Bar(data);
+      generateLegend('legend-6-container', data.datasets);
+    })
+    .catch(function(err) {
+      console.error(err.stack);
+    });
+  }
+
 
   /**
-   * Render the dataChart on the page whenever a new view is selected.
+   * Extend the Embed APIs `gapi.analytics.report.Data` component to
+   * return a promise the is fulfilled with the value returned by the API.
+   * @param {Object} params The request parameters.
+   * @return {Promise} A promise.
    */
-  viewSelector.on('change', function(ids) {
-    dataChart.set({query: {ids: ids}}).execute();
-    newUsersDataChart.set({query: {ids:ids}}).execute();
-    usersDataChart.set({query: {ids:ids}}).execute();
-    sessionsByBrowsersDataChart.set({query: {ids:ids}}).execute();
-    topLandingDataChart.set({query: {ids:ids}}).execute();
-    allPagesDataChart.set({query: {ids:ids}}).execute();
-  });
+  function query(params) {
+    return new Promise(function(resolve, reject) {
+      var data = new gapi.analytics.report.Data({query: params});
+      data.once('success', function(response) { resolve(response); })
+          .once('error', function(response) { reject(response); })
+          .execute();
+    });
+  }
+
+
+  /**
+   * Create a new canvas inside the specified element. Set it to be the width
+   * and height of its container.
+   * @param {string} id The id attribute of the element to host the canvas.
+   * @return {RenderingContext} The 2D canvas context.
+   */
+  function makeCanvas(id) {
+    var container = document.getElementById(id);
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+
+    container.innerHTML = '';
+    canvas.width = container.offsetWidth;
+    canvas.height = container.offsetHeight;
+    container.appendChild(canvas);
+
+    return ctx;
+  }
+
+  /**
+   * Create a visual legend inside the specified element based off of a
+   * Chart.js dataset.
+   * @param {string} id The id attribute of the element to host the legend.
+   * @param {Array.<Object>} items A list of labels and colors for the legend.
+   */
+  function generateLegend(id, items) {
+    var legend = document.getElementById(id);
+    legend.innerHTML = items.map(function(item) {
+      var color = item.color || item.fillColor;
+      var label = item.label;
+      return '<li><i style="background:' + color + '"></i>' + label + '</li>';
+    }).join('');
+  }
+
+  // Set some global Chart.js defaults.
+  Chart.defaults.global.animationSteps = 60;
+  Chart.defaults.global.animationEasing = 'easeInOutQuart';
+  Chart.defaults.global.responsive = true;
+  Chart.defaults.global.maintainAspectRatio = false;
 
 });
