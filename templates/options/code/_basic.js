af38ff6439b6fe3ccdf6312441548c1fb836e956
Daniel Palomino <dapalominop@gmail.com>
2016-12-01 18:25:02 -0500

    Reducido cantidad de países sobre mapa a 31.

--- templates/options/code/_basic.js
+++ templates/options/code/_basic.js
@@ -124,7 +124,7 @@ gapi.analytics.ready(function() {
       'start-date': '31daysAgo',
       'end-date': 'yesterday',
       'sort': '-ga:sessions',
-      'max-results': '41'
+      'max-results': '31'
     },
     chart: {
       container: 'country-container',
