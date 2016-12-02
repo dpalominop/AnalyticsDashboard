f39eeb41e44c97a8f63b526f4aa072febd5ea2e6
Daniel Palomino <dapalominop@gmail.com>
2016-12-02 16:39:59 -0500

    Integrado barras de selección de vista y de fecha de inicio y fin de consulta. Integrado mapa de visualización de sesiones en el mundo.

--- templates/options/code/_channels-keywords.js
+++ templates/options/code/_channels-keywords.js
@@ -18,4 +18,69 @@ gapi.analytics.ready(function() {
     'end-date': '1daysAgo'
   };
 
+  /**
+   * Create a new ViewSelector2 instance to be rendered inside of an
+   * element with the id "view-selector-container".
+   */
+  var viewSelector = new gapi.analytics.ext.ViewSelector2({
+    container: 'view-selector-container',
+  })
+  .execute();
+
+  /**
+   * Create a new DateRangeSelector instance to be rendered inside of an
+   * element with the id "date-range-selector-container", set its date range
+   * and then render it to the page.
+   */
+  var dateRangeSelector = new gapi.analytics.ext.DateRangeSelector({
+    container: 'date-range-selector-container'
+  })
+  .set(dateRange)
+  .execute();
+
+  /**
+   * Update the activeUsers component, the Chartjs charts, and the dashboard
+   * title whenever the user changes the view.
+   */
+  viewSelector.on('viewChange', function(data) {
+    var title = document.getElementById('view-name');
+    title.innerHTML = data.property.name + ' (' + data.view.name + ')';
+
+    // Render all the of charts for this view.
+    countryChart.set({query: {ids: data.ids}}).execute();
+  });
+
+  /**
+   * Register a handler to run whenever the user changes the date range from
+   * the first datepicker. The handler will update the first dataChart
+   * instance as well as change the dashboard subtitle to reflect the range.
+   */
+  dateRangeSelector.on('change', function(data) {
+    var options = {query: data};
+
+    // Render all the of charts for this view.
+    countryChart.set(options).execute();
+
+    // Update the "period" dates text.
+    var datefield = document.getElementById('period');
+    datefield.innerHTML = data['start-date'] + '&mdash;' + data['end-date'];
+  });
+
+  var countryChart = new gapi.analytics.googleCharts.DataChart({
+    query: {
+      metrics: 'ga:sessions',
+      dimensions: 'ga:country',
+      'start-date': '31daysAgo',
+      'end-date': 'yesterday',
+      'sort': '-ga:sessions',
+      'max-results': '21'
+    },
+    chart: {
+      container: 'country-container',
+      type: 'GEO',
+      options: {
+        width: '100%'
+      }
+    }
+  });
 });
