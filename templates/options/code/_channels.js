4762f94b2f4002d5a236d15ad6844a652aac0075
Daniel Palomino <dapalominop@gmail.com>
2016-12-02 16:17:56 -0500

    Agrago template y js de nueva vista de channels.

---
+++ templates/options/code/_channels.js
@@ -0,0 +1,21 @@
+gapi.analytics.ready(function() {
+
+  /**
+   * Authorize the user immediately if the user has already granted access.
+   * If no access has been created, render an authorize button inside the
+   * element with the ID "embed-api-auth-container".
+   */
+  gapi.analytics.auth.authorize({
+    container: 'embed-api-auth-container',
+    clientid: 'REPLACE WITH YOUR CLIENT ID'
+  });
+
+  /**
+   * Query params representing the first chart's date range.
+   */
+  var dateRange = {
+    'start-date': '31daysAgo',
+    'end-date': '1daysAgo'
+  };
+
+});
