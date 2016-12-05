e3c8da3ef8acb365d0ad01581ef4e1af9f08bb01
Daniel Palomino <dapalominop@gmail.com>
2016-12-04 23:31:23 -0500

    Agregado tabla de landingPagePath unido a los eventos del sesiones por país.

--- templates/options/code/_channels-keywords.js
+++ templates/options/code/_channels-keywords.js
@@ -38,34 +38,6 @@ gapi.analytics.ready(function() {
   .set(dateRange)
   .execute();
 
-  /**
-   * Update the activeUsers component, the Chartjs charts, and the dashboard
-   * title whenever the user changes the view.
-   */
-  viewSelector.on('viewChange', function(data) {
-    var title = document.getElementById('view-name');
-    title.innerHTML = data.property.name + ' (' + data.view.name + ')';
-
-    // Render all the of charts for this view.
-    countryChart.set({query: {ids: data.ids}}).execute();
-  });
-
-  /**
-   * Register a handler to run whenever the user changes the date range from
-   * the first datepicker. The handler will update the first dataChart
-   * instance as well as change the dashboard subtitle to reflect the range.
-   */
-  dateRangeSelector.on('change', function(data) {
-    var options = {query: data};
-
-    // Render all the of charts for this view.
-    countryChart.set(options).execute();
-
-    // Update the "period" dates text.
-    var datefield = document.getElementById('period');
-    datefield.innerHTML = data['start-date'] + '&mdash;' + data['end-date'];
-  });
-
   var countryChart = new gapi.analytics.googleCharts.DataChart({
     query: {
       metrics: 'ga:sessions',
@@ -83,4 +55,123 @@ gapi.analytics.ready(function() {
       }
     }
   });
+
+  /**
+   * Create a table chart showing Top Landing Page over time for the country the
+   * user selected in the main chart.
+   */
+  var landingPathChart = new gapi.analytics.googleCharts.DataChart({
+    query: {
+      'metrics': 'ga:sessions,ga:users',
+      'dimensions': 'ga:landingPagePath',
+      'start-date': '31daysAgo',
+      'end-date': 'yesterday',
+      'sort': '-ga:users',
+      'max-results': '10'
+    },
+    chart: {
+      type: 'TABLE',
+      container: 'landingpath-chart-container',
+      options: {
+        width: '100%'
+      }
+    }
+  });
+
+  /**
+   * Store a refernce to the row click listener variable so it can be
+   * removed later to prevent leaking memory when the chart instance is
+   * replaced.
+   */
+  var countryChartClickListener;
+  var country;
+  /**
+   * Update the activeUsers component, the Chartjs charts, and the dashboard
+   * title whenever the user changes the view.
+   */
+  viewSelector.on('viewChange', function(data) {
+    var title = document.getElementById('view-name');
+    title.innerHTML = data.property.name + ' (' + data.view.name + ')';
+
+    var options = {query: {ids: data.ids}};
+
+    // Clean up any event listeners registered on the main chart before
+    // rendering a new one.
+    if (countryChartClickListener) {
+      google.visualization.events.removeListener(countryChartClickListener);
+    }
+
+    // Render all the of charts for this view.
+    countryChart.set(options).execute();
+    landingPathChart.set(options);
+
+    // Only render the breakdown chart if a Country filter has been set.
+    if (landingPathChart.get().query.filters) landingPathChart.execute();
+  });
+
+  /**
+   * Register a handler to run whenever the user changes the date range from
+   * the first datepicker. The handler will update the first dataChart
+   * instance as well as change the dashboard subtitle to reflect the range.
+   */
+  dateRangeSelector.on('change', function(data) {
+    var options = {query: data};
+
+    // Clean up any event listeners registered on the main chart before
+    // rendering a new one.
+    if (countryChartClickListener) {
+      google.visualization.events.removeListener(countryChartClickListener);
+    }
+
+    // Render all the of charts for this view.
+    countryChart.set(options).execute();
+    landingPathChart.set(options);
+
+    // Only render the breakdown chart if a Country filter has been set.
+    if (landingPathChart.get().query.filters) landingPathChart.execute();
+
+    // Update the "period" dates text.
+    var datefield = document.getElementById('period');
+    datefield.innerHTML = data['start-date'] + '&mdash;' + data['end-date'];
+  });
+
+  /**
+   * Each time the main chart is rendered, add an event listener to it so
+   * that when the user clicks on a row, the line chart is updated with
+   * the data from the browser in the clicked row.
+   */
+  countryChart.on('success', function(response) {
+    var chart = response.chart;
+    var dataTable = response.dataTable;
+
+    // Store a reference to this listener so it can be cleaned up later.
+    countryChartClickListener = google.visualization.events
+        .addListener(chart, 'select', function(event) {
+
+      // When you unselect a row, the "select" event still fires
+      // but the selection is empty. Ignore that case.
+      if (!chart.getSelection().length) return;
+
+      var row =  chart.getSelection()[0].row;
+      country =  dataTable.getValue(row, 0);
+      var options = {
+        query: {
+          filters: 'ga:country==' + country
+        },
+        chart: {
+          options: {
+            title: country
+          }
+        }
+      };
+
+      landingPathChart.set(options).execute();
+    });
+  });
+
+  // Set some global Chart.js defaults.
+  Chart.defaults.global.animationSteps = 60;
+  Chart.defaults.global.animationEasing = 'easeInOutQuart';
+  Chart.defaults.global.responsive = true;
+  Chart.defaults.global.maintainAspectRatio = false;
 });
